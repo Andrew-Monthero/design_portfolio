e259493899828c138aa593c33b8c954f6cd762f7
Andrew <andrewmonthero0210@gmail.com>
2023-09-19 18:35:02 +0530

    eight commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,7 @@ export default function Hero() {
       <div className='absolute h-screen top-0 bottom-0 left-0 right-0 bg-black/80 z-0' />
       <div className='h-screen flex justify-center flex-col mx-[1rem] md:max-w-screen-lg md:mx-auto'>
         <h1 className='text-[2.5rem] md:text-[5rem] text-center leading-snug tracking-normal z-[2] font-medium  text-purple-500'>
-          Transforming <span className='italic font-bold '>ideas</span> into{' '}
-          <span className='italic font-semibold '>impactful</span> experiences
-          through <span className='italic font-semibold '>thoughtful</span>{' '}
+          Transforming ideas into impactful experiences through thoughtful
           design solutions.
         </h1>
         <p className='text-[1.125rem] mt-[1rem] font-light leading-relaxed tracking-wide z-[2] md:max-w-screen-sm text-center mx-auto'>
